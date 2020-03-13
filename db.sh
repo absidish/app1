458232746e162477b2adda2f77dffef7a8037849
Sydyk Akhmataliev <sahmataliev@gmail.com>
2020-03-14 05:47:03 +0600

    Update db.sh

--- db.sh
+++ db.sh
@@ -2,7 +2,7 @@
 
 mysql -u root  --execute="create database ofbiz;";
 mysql -u root  --execute="create database ofbizolap;";
-mysql -u root  --execute="create database create database ofbiztenant;";
+mysql -u root  --execute="create database ofbiztenant;";
 
 mysql ofbiz < /opt/ofbiz.sql;
 mysql ofbizolap < /opt/ofbizolap.sql;
