357c9da157d9020c484eb4a3987c36200d8b90c2
Sydyk Akhmataliev <sahmataliev@gmail.com>
2020-03-14 05:25:54 +0600

    Create db.sh

---
+++ db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysql -u root  --execute="create database ofbiz;";
+mysql -u root  --execute="create database ofbizolap;";
+mysql -u root  --execute="create database create database ofbiztenant;";
+
+mysql ofbiz < /opt/ofbiz.sql;
+mysql ofbizolap < /opt/ofbizolap.sql;
+mysql ofbiztenant < /opt/ofbiztenant.sql;
