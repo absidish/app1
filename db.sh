dac476bee711ae9bc3d9d5a706de6bcf3bb1cbc5
Sydyk Akhmataliev <sahmataliev@gmail.com>
2020-03-14 06:09:22 +0600

    Update db.sh

--- db.sh
+++ db.sh
@@ -4,6 +4,9 @@ mysql -u root  --execute="create database ofbiz;";
 mysql -u root  --execute="create database ofbizolap;";
 mysql -u root  --execute="create database ofbiztenant;";
 
+mysql -u root  --execute="CREATE USER 'ofbiz'@'%' IDENTIFIED BY 'ofbiz';"
+mysql -u root  --execute="GRANT ALL PRIVILEGES ON *.* TO 'ofbiz'@'%';"
+
 mysql ofbiz < /opt/ofbiz.sql;
 mysql ofbizolap < /opt/ofbizolap.sql;
 mysql ofbiztenant < /opt/ofbiztenant.sql;
